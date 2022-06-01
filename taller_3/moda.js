6bbfbd9de9a5c97ef228e4c3df4439af83384d35
Andres Badillo <r.andres.badillo@gmail.com>
2022-05-31 21:27:18 -0500

    Se agregó el cálculo de la moda al taller 3

---
+++ taller_3/moda.js
@@ -0,0 +1,28 @@
+function calculateModa() {
+    const listaNumbers = document.getElementById("inputNumbers").value;
+    let arrayNumbers = Array.from(listaNumbers.split(","),Number);
+
+    const listaCount = {};
+
+    arrayNumbers.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
+        }
+    );
+
+    const listaArray = Object.entries(listaCount).sort(
+        function (a, b) {return a[1] - b[1];}
+    );
+    
+    const moda = listaArray[listaArray.length - 1];
+
+    const finalResult = document.getElementById("FinalValue");
+    finalResult.innerText = "La moda es el número: " + moda[0];
+}
+
+
+
