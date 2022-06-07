65ac2a53a8559343ef1d64d653276442ad96156f
Andres Badillo <r.andres.badillo@gmail.com>
2022-06-06 19:20:09 -0500

    Se carga taller 2 - Promedio Ponderado

--- taller_3/ponderado.js
+++ taller_3/ponderado.js
@@ -1,7 +1,5 @@
-/* Creo los Array */
-const arrayMaterias = [];
-const arrayNumbers = [];
-const arrayCredits = [];
+/* Creo el Array */
+const arrayCalificacion = [];
 
 function loadArray() {
     /* Leo los datos escritos en el input y los cargo */
@@ -9,14 +7,53 @@ function loadArray() {
     const listaMaterias = materias.selectedOptions[0].text;
     const listaNumbers = document.getElementById("inputNumber").value;
     const listaCredits = document.getElementById("inputCredits").value;
+    
+    /* Creo el objeto */
+    const calificacionPonderada = new Object();
+    calificacionPonderada.materia = listaMaterias;
+    calificacionPonderada.nota = Number(listaNumbers);
+    calificacionPonderada.creditos = Number(listaCredits);
 
     /* Empujo los datos convertidos en número (incluido decimal) al final del array */
-    arrayMaterias.push(listaMaterias);
-    arrayNumbers.push(Number(listaNumbers));
-    arrayCredits.push(Number(listaCredits));
+    arrayCalificacion.push(calificacionPonderada);
 
-    console.log(arrayMaterias);
-    console.log(arrayNumbers);
-    console.log(arrayCredits);
+    console.log(arrayCalificacion);
+}
+
+function calculatePonderado() {
+    /* Multiplicar cada nota por su peso en créditos */
+    const notesWithCredit = arrayCalificacion.map(
+        function (noteObject) {
+            return noteObject.nota * noteObject.creditos;
+        }
+    );
+
+    /* Sumo todos los elementos de las notas multiplicadas por su peso en créditos */
+    const sumOfNotesWithCredit = notesWithCredit.reduce(
+        function (sum = 0, newVal) {
+            return sum + newVal;
+        }
+    );
+
+    /* Creamos un arreglo solo con los créditos */
+    const credits = arrayCalificacion.map(
+        function (noteObject) {
+            return noteObject.creditos;
+        }
+    );
+
+    /* Sumamos los créditos */
+    const sumOfCredits = credits.reduce(
+        function (sum = 0, newVal) {
+            return sum + newVal;
+        }
+    );
+
+    /* Dividimos ambas sumas */
+    const promedioPonderado = (sumOfNotesWithCredit / sumOfCredits).toFixed(2);
+    
+    /* Publicamos el resultado */
+    const finalResult = document.getElementById("FinalValue");
+    finalResult.innerText = "El promedio ponderado es: " + promedioPonderado;
 }
 
