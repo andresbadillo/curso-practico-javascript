6afceb9b72d3a9c5b2f2692d4fb361e91cdfa819
Andres Badillo <r.andres.badillo@gmail.com>
2022-06-07 20:43:26 -0500

    Se creo el taller #4 y la pagina principal index.html

---
+++ taller_4/salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Andres",
+    salary: 3000,
+});
+colombia.push({
+    name: "Natalia",
+    salary: 1500,
+});
+colombia.push({
+    name: "Esteban",
+    salary: 1600,
+});
+colombia.push({
+    name: "Daniela",
+    salary: 1200,
+});
+colombia.push({
+    name: "José",
+    salary: 800,
+});
+colombia.push({
+    name: "Adriana",
+    salary: 1700,
+});
+colombia.push({
+    name: "Joaquin",
+    salary: 1900,
+});
+colombia.push({
+    name: "Luisa",
+    salary: 800,
+});
+colombia.push({
+    name: "Adrian",
+    salary: 1100,
+});
+colombia.push({
+    name: "Kennya",
+    salary: 1400,
+});
+colombia.push({
+    name: "Ramiro",
+    salary: 1900,
+});
+colombia.push({
+    name: "Camila",
+    salary: 1000,
+});
+colombia.push({
+    name: "Monica",
+    salary: 1600,
+});
+colombia.push({
+    name: "Pedro",
+    salary: 700,
+});
+colombia.push({
+    name: "Pablo",
+    salary: 1700,
+});
+colombia.push({
+    name: "Heidy",
+    salary: 1400,
+});
+colombia.push({
+    name: "Dayro",
+    salary: 1200,
+});
+colombia.push({
+    name: "Dayana",
+    salary: 1600,
+});
+colombia.push({
+    name: "Leonardo",
+    salary: 1300,
+});
+colombia.push({
+    name: "Diana",
+    salary: 1800,
+});
